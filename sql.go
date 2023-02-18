32fe7e0fcd4cc1fdd43c15d95064b443209f2dd6
Damien Buttler <damien@doublehops.com>
2023-02-18 14:48:11 +1100

    Updated query to check if migrations table exists.

--- sql.go
+++ sql.go
@@ -2,35 +2,27 @@ package go_migration
 
 const QuerySeparator = "------------------"
 
-var GetLatestMigrationSQL =
-	`SELECT * FROM migrations
+var GetLatestMigrationSQL = `SELECT * FROM migrations
 	ORDER BY id DESC
 	LIMIT 1
 `
 
-var CreateMigrationsTable =
-	`CREATE TABLE migrations (
+var CreateMigrationsTable = `CREATE TABLE migrations (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	filename VARCHAR(255),
 	created_at DATETIME,
 	PRIMARY KEY(id)
 )`
 
-var CheckMigrationsTableExistsSQL =
-	`SELECT TABLE_NAME AS tables
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE TABLE_SCHEMA = 'cw' AND TABLE_NAME = 'migrations'
+var CheckMigrationsTableExistsSQL = `SHOW TABLES
 `
 
-var InsertMigrationRecordIntoTableSQL =
-	`INSERT INTO migrations 
+var InsertMigrationRecordIntoTableSQL = `INSERT INTO migrations 
 	(filename,created_at)
 	VALUES
 	(?,NOW())
 `
 
-var RemoveMigrationRecordFromTableSQL =
-	`DELETE FROM migrations 
+var RemoveMigrationRecordFromTableSQL = `DELETE FROM migrations 
 	WHERE filename = ?
 `
-
